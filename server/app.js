2e14f25a6884b56361ad34592d3c925057e37a6a
Joafie <jade424433@gmail.com>
2018-08-02 14:46:46 -0700

    Serving static react build now

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
-const Joi = require('joi');
 var express = require('express');
 var app = express();
+const path = require('path');
 app.use(express.json());
 require('./routes')(app);
+app.use('/projects/bank-ledger', express.static(path.join(__dirname, './build')));
 
-const port = process.env.PORT || 5000;
+const port = 5000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
