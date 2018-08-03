f9cf848d250c3c0e5d53bb5c5e93481c65a12a39
Joafie <jade424433@gmail.com>
2018-08-02 18:07:55 -0700

    fixed url routes

--- client/src/Components/LoginSignup/LoginSignupShell.js
+++ client/src/Components/LoginSignup/LoginSignupShell.js
@@ -32,7 +32,7 @@ class LoginSignupShell extends Component {
         };
 
         if (isSignup) {
-            fetch("/bank-ledger/api/createUser", options)
+            fetch("/projects/bank-ledger/api/createUser", options)
                 .then(res => res.json())
                 .then(payload => {
                     if (payload.message) { // If signup is successful...
@@ -44,7 +44,7 @@ class LoginSignupShell extends Component {
                     }
                 });
         } else {
-            fetch("/bank-ledger/api/login", options)
+            fetch("/projects/bank-ledger/api/login", options)
                 .then(res => res.json())
                 .then(payload => {
                     if (payload.token) { // If login was successful...
--- server/routes.js
+++ server/routes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
     let amountRegex = /^[0-9]\d*(()?(\.\d{0,2})?)$/;
 
     // The user creation endpoint. Inserts in users table given validation passes.
-    app.post('/bank-ledger/api/createUser', (req, res) => {
+    app.post('/api/createUser', (req, res) => {
         let result = valid.validateSignup(req.body);
         if (result) return res.status(400).send(result);
         db.insertUser(req.body)
@@ -16,21 +16,21 @@ module.exports = function (app) {
     });
 
     // The login endpoint. Returns a token and user id given validation passes.
-    app.post('/bank-ledger/api/login', (req, res) => {
+    app.post('/api/login', (req, res) => {
         let result = valid.validateLogin(req.body);
         if (result) return res.status(400).send(result);
         res.status(200).send(db.assignToken(req.body.email));
     });
 
     // Gets the balance and transactions for a user given token validation passes.
-    app.post('/bank-ledger/api/getBalance', (req, res) => {
+    app.post('/api/getBalance', (req, res) => {
         let user = db.checkToken(req.body.id, req.body.token);
         if (!user) return res.status(400).send({ message: "Invalid token" });
         return res.status(200).send({ balance: user.balance, transactions: db.getTransactions(user.id) });
     });
 
     // Inserts a deposit transaction into the transaction table given token validation passes.
-    app.put('/bank-ledger/api/makeDeposit', (req, res) => {
+    app.put('/api/makeDeposit', (req, res) => {
         let user = db.checkToken(req.body.id, req.body.token);
         if (!amountRegex.test(req.body.depositAmount) || !user) return res.status(400).send({ message: "Invalid token or format!" });
         user.balance = Math.round((user.balance + parseFloat(req.body.depositAmount)) * 100) / 100;
@@ -38,7 +38,7 @@ module.exports = function (app) {
     });
         
     // Inserts a withdraw transaction into the transaction table given token validation passes.
-    app.put('/bank-ledger/api/makeWithdrawl', (req, res) => {
+    app.put('/api/makeWithdrawl', (req, res) => {
         let user = db.checkToken(req.body.id, req.body.token);
         if (!amountRegex.test(req.body.withdrawAmount) || !user) return res.status(400).send({ message: "Invalid token or format!" });
         user.balance = Math.round((user.balance - parseFloat(req.body.withdrawAmount)) * 100) / 100;
