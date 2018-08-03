58e51998937d22323d1347bb561730ca9eefe399
Joafie <jade424433@gmail.com>
2018-08-02 18:32:47 -0700

    removed path

--- server/bank-ledger.js
+++ server/bank-ledger.js
@@ -3,7 +3,7 @@ var app = express();
 const path = require('path');
 app.use(express.json());
 require('./routes')(app);
-app.use('/projects/bank-ledger', express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, './build')));
 
 const port = 5000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
